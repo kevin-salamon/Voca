1b55de532ac87bfdd704a9dad3bb61ce65bda0d5
kevin-salamon <kevinwebdevs@gmail.com>
2020-04-25 00:29:45 -0400

    added basic component for job list

--- http/web/client/src/Homepage.js
+++ http/web/client/src/Homepage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./style.css";
 import { saveJob, getSavedJobs, removeJob } from "./utils/API";
 import JobModal from "./components/JobModal";
+import JobRow from "./components/JobRow.js";
 
 class Homepage extends React.Component {
 
@@ -14,11 +15,17 @@ class Homepage extends React.Component {
       return (
         <div>
           <nav className="navbar my-nav">
-            <div className="nav-title">Jtrackr</div>
+            <div className="nav-title">Voca</div>
             <JobModal />
           </nav>
           <div className="job-area text-center">
-            <h1 className="empty-jobs">No jobs here now. Add a job to get started!</h1>
+            <JobRow
+            title="Test Title"
+            employer="Test Employer"
+            location="Test Location"
+            notes="GIVE ME THE MEATS"
+            followup="Test Tomorrow"
+            />
           </div>
         </div>
       );
